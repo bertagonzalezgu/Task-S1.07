eb52cb5276120eb756676846e3ee82b419455a63
bertagonzalezgu <berta.gonzalez99@gmail.com>
2026-04-27 17:24:26 +0200

    fix(03_new-apis.test): 03_new-apis-5 modified

--- exercises/03_new-apis.test.js
+++ exercises/03_new-apis.test.js
@@ -23,6 +23,7 @@ test('03_new-apis-3: should be able to take an array-like object and convert it 
 test('03_new-apis-4: should be easier to fill an array with values', () => {
   const originalArray = new Array(5)
   // crea una variable anomenada `result` i assigna-li un array omplert amb 3s excepte el primer element
+  const result = originalArray.fill(3, 1)
   expect(result).toEqual([, 3, 3, 3, 3]) // eslint-disable-line no-sparse-arrays
 })
 
@@ -54,6 +55,8 @@ test('03_new-apis-5: should be easy to copy properties from one object to anothe
     p: ['x', 'y', 'z'],
   }
   // combina les fonts dins del target utilitzant Object.assign
+
+  const result = Object.assign(target, source1,  source2, source3)
 
   expect(result).toEqual({
     a: {
